48ad23a5a519da8d7d4ad13434ef2c87a084d755
Baawaah <son.tran.be@gmail.com>
2016-11-26 12:29:46 +0100

    welp let's get to work

---
+++ SR-71/main.m
@@ -0,0 +1,17 @@
+%%
+% Thanh-Son TRAN
+%
+%%
+% Flight Condition
+z     = 20000      ; %[m]
+p0    = 5475       ; %[Pa]   
+T0    = 216        ; %[K]
+rho   = 0.088      ; %[kg/m^3]
+gamma = 1.4        ;
+Pi_c  = 20         ;
+T_t4  = 1400       ; %[K]
+T_t7  = 2300       ; %[K]
+LHV   = 43.19*10^6 ; %[K]
+%%
+% Equation box
+
